988239a4046776c38f1849ef11dceaa55e957f4d
laadiim <112978764+laadiim@users.noreply.github.com>
2024-09-09 13:19:44 +0200

    Splitting code into methods

--- parser.js
+++ parser.js
@@ -6,6 +6,9 @@ def check_even_numbers(numbers):
             even_numbers.append(number)
     return even_numbers
 
+def func(foo, bar):
+    print(foo + bar)
+
 numbers_list = [1, 2, 3, 4, 5, 6]
 result = check_even_numbers(numbers_list)
 print("Even numbers are:", result)
